b3b6f14ce41a1ef575dc5dbfb0c671c7565a8049
pchittum <pchittum@salesforce.com>
2014-09-24 13:20:43 +0100

    added lab3.html and basic node server server.js

---
+++ labs/server.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var fs = require('fs');
+var svr = http.createServer(function(req, res){
+	console.log(req.url);
+	var file = req.url === '/' ? 'lab3.html' : './'+req.url; 
+
+	fs.readFile(file, function(err,data){
+		if (err){
+			res.writeHead(404);
+			res.write(JSON.stringify(err));
+			return res.end();
+		} else {
+			res.writeHead(200);
+			res.write('Something');
+			return res.end();
+		}
+	});
+});
+
+var port = process.argv[2];
+
+svr.listen(8000);
+
+
+
+
